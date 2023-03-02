205646c4a71816492fb3f72907472d4f9afd12dd
joaolorda <123481493+joaolorda@users.noreply.github.com>
2023-03-01 21:09:45 -0300

    Add files via upload

---
+++ ja/Untitled2.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main(){
+
+    int num;
+    
+    printf("DIGITE UM NUMERO INTEIRO: ");
+    
+    scanf("%d",&num); 
+    
+    printf("O VALOR DIGITADO FOI: %d",num);
+
+
+
+
+
+     return 0;
+
+}
